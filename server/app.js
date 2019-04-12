a6e2214a12d576122646f761bfbe0f5fa1529fbb
Jordan Machalek <jdm4344@rit.edu>
2019-04-11 23:47:43 -0400

    fixed favicon links

--- server/app.js
+++ server/app.js
@@ -14,7 +14,7 @@ const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/kwikpost';
 
 mongoose.connect(dbURL, (err) => {
   if (err) {
@@ -41,7 +41,7 @@ const router = require('./router.js');
 const app = express();
 
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.jpg`));
+app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
